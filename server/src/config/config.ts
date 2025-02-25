2511eb07894ebde132d90169fe9a498c742b2e6f
vishal <dasvishal60@gmail.com>
2025-02-26 01:52:53 +0530

    minor changes

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -6,12 +6,8 @@ const CONFIG = {
   MONGODB_URL: process.env.MONGODB_URL as string,
 
   // JWT
-  ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY ,
+  ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY,
   ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET as string,
-
-  //Nodemailer
-  Auth_MAIL: process.env.Auth_MAIL as string,
-  Auth_PASS: process.env.Auth_PASS as string,
 };
 
 export default CONFIG;
